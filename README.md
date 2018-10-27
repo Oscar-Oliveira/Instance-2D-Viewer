64538420668c99d379ff0f5f6dd6cf216191a35c
Oscar Oliveira <oscar.m.oliveira@gmail.com>
2018-10-27 09:06:11 +0100

    updated

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 Drag and drop JSON File to Drop Zone (top left corner)
 
 Example data contains 5 instances from:
+
 * Wang, P. Y. (1983). Two Algorithms for Constrained Two-Dimensional Cutting Stock Problems. Operations Research, 31(3), 573–586. http://doi.org/10.1287/opre.31.3.573
 
 [Screenshot](https://github.com/Oscar-Oliveira/InstanceViewer2D/blob/master/images/InstanceViewer.png)
--- scripts/help.js
+++ scripts/help.js
@@ -1,18 +1,16 @@
- var z = function () {
+var z = function () {
     var iDiv = document.createElement('div');
-	iDiv.appendChild( doP("> Author", 'popupTitle') );
+    iDiv.appendChild( doP("> Author", 'popupTitle') );
     p = doP("Name: ", 'popupText');
-	var element = document.createElement('a');
+    var element = document.createElement('a');
     element.href = "https://www.linkedin.com/in/oscaroliveira";
     element.textContent = "Óscar Oliveira";
     element.target = "_blank";
     p.appendChild(element, '');
     iDiv.appendChild( p );
     iDiv.appendChild( doP("Email: oscar.m.oliveira@gmail.com", 'popupText') );
-	
-	iDiv.appendChild( doP("> Help", 'popupTitle') );
+    iDiv.appendChild( doP("> Help", 'popupTitle') );
     iDiv.appendChild( doP("Drop Instance file (JSON format) to the left corner drop zone to visualize the instance.", 'popupText') );
-
     document.getElementById("popup").appendChild(iDiv);
 };
 
@@ -22,25 +20,25 @@ document.getElementById("show").onclick = (function() {
     } else { document.getElementById("opacity-provider").style.display = "block"; }
 });
 
-function doP(text, className) {            
+function doP(text, className) {
     var element = document.createElement('p');
     element.textContent = text;
-    element.className = className;            
+    element.className = className;
     return element;
 }
 
-function doPre(text, className) {            
+function doPre(text, className) {
     var element = document.createElement('pre');
     element.textContent = text;
-    element.className = className;            
+    element.className = className;
     return element;
 }
 
-function doA(text, className) {            
+function doA(text, className) {
     var element = document.createElement('a');
     element.textContent = text;
     element.href = text;
-    element.className = className;            
+    element.className = className;
     return element;
 }
 
